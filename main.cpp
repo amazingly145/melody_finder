3972a7f1ada9299573c0c6b18ad8e4b280b1a5ed
Andrea_Cantu <A01753419@tec.mx>
2025-12-03 11:32:19 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,3 @@
-/* 
- * Autor: Andrea Iliana Cantu Mayorga 
- * Matricula: A01753419
- * Fecha: 3 de diciembre del 2025
- * Materia: programacion de estructura de datos y algoritmos*/
 using namespace std;
 #include <iostream>
 #include <string>
@@ -12,27 +7,6 @@ using namespace std;
 #include "music.h"
 #include <fstream>
 
-/*guardar CSV
-Funcion para que despues de agregar nuevos artistas
-se puedan guardar en el CSV*/
-void guardarCSV(vector<vector<string>> &matriz) {
-    //Abrimos el archivo .csv
-    ofstream archivo(DATA_SET);
-    //Nombres de los encabezados que vamos a guardar los artistas
-    archivo << "artists,album,track\n";
-    //Obtenemos las filas de la matriz
-    for (int i = 0; i < matriz.size(); i++) {
-        archivo << matriz[i][0] << "," 
-        << matriz[i][1] << "," 
-        << matriz[i][2] << "\n";
-}
-    //Cerramos el archivo
-    archivo.close();
-}
-
-/*Funcion de menu
- * El menu de opciones que el usuaario tiene 
- * para hacerle modificaciones al archivo*/
 void menu(){
     cout << "-----Menu------" << endl;
     //Funciones con double list
@@ -42,8 +16,7 @@ void menu(){
     cout << "3. Ordenar alafabeticamente los artistas" << endl;
     cout << "4. Ordenar alfabeticamente las canciones" << endl;
     cout << "5. Ordenar alfabeticamente los albumes" << endl;
-    cout << "6. Visualizar base de datos" << endl;
-    cout << "7. Salida" << endl;
+    cout << "6. Salida" << endl;
 }
 
 int main(){   
@@ -53,21 +26,21 @@ int main(){
     string artistas;
     string cancion;
     string album;
-    int agregar;
     //Creamos la matriz de musica con todos los datos
     vector<vector<string>> matriz_musica;
-    //Variables para subir el .csv al programa
+    //dataset upload
     ifstream music_dataset(DATA_SET);
     string linea;
     string valores;
     char delimitador = ',';
-    //Funcion para hacer el Quick Sort
+    //QUICK SORT
     Sort<string>ordered_music;
-    //Para la lista doblemente ligada
+    //DOUBLE LINKED LIST
     DList<string> double_list_music;
-    //leemos la primer linea
+    vector<string> vector_temporal;
+    //we read the first line
     getline(music_dataset, linea);
-    //leemos todas las columnas de nuestro archivo
+    //We read all the lines
     while (getline(music_dataset, linea)){
         stringstream stream(linea);
         string id, track_id, artists, album_name, track_name, popularity, duration_ms,
@@ -79,33 +52,28 @@ int main(){
         getline(stream, artists, delimitador);
         getline(stream, album_name, delimitador);
         getline(stream, track_name, delimitador);
-        //matriz principal que se a utilizrar principalmente para el sorteo
+        //matriz
         matriz_musica.push_back({artists, album_name, track_name});
-        //agregamos los valores a la lista doblemente ligada 
+        //Listas doblemente ligadas
         double_list_music.add(artists, album_name, track_name);
     }
-    cout << "-----Bienvenido a Melody Finder-----" << endl;
+    cout << "-----Bienvenido al sistema de musica-----" << endl;
     while(consultante == true){
         menu();
         cout << "Selecciona la opcion que deseas usar: "<< endl;
         cin >> usuario;
         if(usuario == 1){
             cout << "------ Agregar nuevos elementos ------" << endl;
-            cout << "Numero de artistas a agregar: " << endl;
-            cin >> agregar;
-            while (agregar > 0){
-                cout << "Escribir el nombre del albumm, artista y cancion la primer letra en mayuscula" << endl;
-                cout << "Nombre de artista nuevo: " << endl;
-                cin >> artistas;
-                cout << "Nombre de album de " << artistas << endl;
-                cin >> album;
-                cout << "Nombre de cancion en album " << album << " Con artista " << artistas << endl;
-                cin >> cancion;
-                double_list_music.add(artistas, album, cancion);
-                matriz_musica = double_list_music.to_vector();
-                guardarCSV(matriz_musica);
-                agregar--;
-            }
+            cout << "Escribir el nombre del albumm, artista y canncion la primer letra en mayuscula" << endl;
+            cout << "Nombre de artista nuevo: " << endl;
+            cin >> artistas;
+            cout << "Nombre de album de " << artistas << endl;
+            cin >> album;
+            cout << "Nombre de cancion en album " << album << " Con artista " << artistas << endl;
+            cin >> cancion;
+            double_list_music.add(artistas, album, cancion);
+            matriz_musica = double_list_music.to_vector();
+
         }
         if(usuario == 2){
             cout << "------Funcion de busqueda------" << endl;
@@ -114,6 +82,7 @@ int main(){
             getline(cin,artistas);
             cout << double_list_music.search(artistas) << endl;
             cout << "------------------------------" << endl;
+
         }
         if(usuario == 3){
             cout << "------Ordenar artistas por orden alfabetico-----" << endl;
@@ -146,15 +115,6 @@ int main(){
             }
         }
         if(usuario == 6){
-            cout << "-----Visualizar la base de datos desordenada-----" << endl;
-            for(int i = 0; i < matriz_musica.size(); i++){
-                cout << "Album: " << matriz_musica[i][1] << endl;
-                cout << "Artista: " << matriz_musica[i][0] << endl;
-                cout << "Cancion: " << matriz_musica[i][2] << endl;
-                cout << "---------------------------------------------" << endl;
-            }
-        }
-        if(usuario == 7){
             cout << "Saliendo del programa..." << endl;
             consultante = false;
         }
